d4563b0e9f113964a58efa86dbecc8439a569a47
mythrayee19 <62176676+mythrayee19@users.noreply.github.com>
2020-09-08 23:39:56 +0530

    Add files via upload

---
+++ day5-3.py
@@ -0,0 +1,5 @@
+#[“hey this is sai”,I am in mumbai”,....]
+#o/p- [“Hey This Is Sai”, I Am In Mumbai”,....]
+list1=["hey this is sai","i am in mumbai,..."]
+ans=map(lambda a:a.title(),list1)
+print(list(ans))
